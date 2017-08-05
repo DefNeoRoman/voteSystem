3b1748ffcc067cd23096844cf07cfb5e917ca623
DefNeoRoman <d89056411060@yandex.ru>
2017-08-05 22:28:32 +0300

    firstCommit

---
+++ src/main/java/com/SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class SpringBootWebApplication extends SpringBootServletInitializer{
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
