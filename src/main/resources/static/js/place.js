99e5d5d11e418d227e03bcd4e855e15e122ec1a3
DefNeoRoman <mistarg852455>
2017-10-19 11:51:10 +0300

    add form

--- src/main/resources/static/js/place.js
+++ src/main/resources/static/js/place.js
@@ -17,13 +17,17 @@ $(function(){
             html+='<tr>'+ //tr - строка
 
             '<td>'+value.name + '<td>'+ // td - столбец
-            '<td>'+
+            '<td>'+ //action="javascript:void(null); штука, чтобы форма не перезагружала страницу
+                '<form method="POST" class="postForm" action="javascript:void(null);" onsubmit="call(this)">'+
                 '<div class="count-input space-bottom">'+
-                    '<a class="incr-btn" data-action="decrease" href="#" onclick="testFunction(this)">–</a>'+
-                        '<input class="quantity" type="text" name="quantity" value="'+value.votes+'"/>'+
-                    '<a class="incr-btn" data-action="increase" href="#" onclick="testFunction(this)">&plus;</a>'+
-                '</div>'+
 
+                    '<a class="incr-btn" data-action="decrease" href="#" onclick="testFunction(this)">–</a>'+
+                        '<input class="quantity" id="pInput" type="text" name="quantity" value="'+value.votes+'"/>'+
+                    '<a class="incr-btn" data-action="increase" href="#" onclick="testFunction(this)">&plus;</a>' +
+                '<input type="submit" value="save" >'+
+
+                '</div>'+
+                '</form>'+
             '</td>';
 
             html+='</tr>';
@@ -36,13 +40,15 @@ $(function(){
 
 });
 function testFunction(d) {
-
+        var msg   = $(d).parent().parent().find('.postForm').serialize();
+        console.log(d);
+        console.log(msg);
         var $button = $(d);
         var oldValue = $(d).parent().find('.quantity').val();
         $button.parent().find('.incr-btn[data-action="decrease"]').removeClass('inactive');
         if ($button.data('action') == "increase") {
             var newVal = parseFloat(oldValue) + 1;
-            console.log(newVal);
+
         } else {
             // Don't allow decrementing below 1
             if (oldValue > 1) {
@@ -57,4 +63,7 @@ function testFunction(d) {
 
 
 }
-
+function call(form) {
+    var msg   = $(form).serialize();
+    console.log(msg);
+}
