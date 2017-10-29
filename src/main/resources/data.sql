095de68ffbf444ae50a9479729ed4912b40c1fae
DefNeoRoman <d89056411060@yandex.ru>
2017-10-29 23:22:57 +0300

    many-to-many notworking

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,21 @@
 DELETE FROM meals;
 DELETE FROM menus;
-DELETE FROM menumeals;
+DELETE FROM restaurants;
 
-INSERT INTO meals (price, name) VALUES
-  (763,'макароны'),
-  (863,'картофель'),
-  (773,'хлеб'),
-  (963,'фрукты'),
-  (1003,'борщ'),
-  (2003,'щи');
-INSERT INTO menus (name, cookname) VALUES
-  ('завтрак','Вова'),
-  ('обед','Петя'),
-  ('ужин','Вася');
 
-INSERT INTO menumeals (menu_id, meal_id) VALUES
-(9,1),
-(9,2),
-(8,3),
-(8,4),
-(7,5),
-(7,6);
+INSERT INTO meals (price, name,menu_id) VALUES
+  (763,'макароны',9),
+  (863,'картофель',8),
+  (773,'хлеб',7),
+  (963,'фрукты',9),
+  (1003,'борщ',8),
+  (2003,'щи',7);
+INSERT INTO menus (name, cookname,restaurant_id) VALUES
+  ('завтрак','Вова',10),
+  ('обед','Петя',11),
+  ('ужин','Вася',12);
+
+INSERT INTO restaurants (name, votes) VALUES
+  ('бар',5),
+  ('гостиница',6),
+  ('пляж',7);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,31 +1,31 @@
 DROP TABLE IF EXISTS meals cascade ;
 DROP TABLE IF EXISTS menus cascade;
-DROP TABLE IF EXISTS menumeals cascade;
+DROP TABLE IF EXISTS restaurants cascade;
 
 DROP SEQUENCE IF EXISTS global_seq;
 CREATE SEQUENCE global_seq START 1;
+CREATE TABLE restaurants
+(
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name        VARCHAR  NOT NULL,
+  votes       INTEGER  NOT NULL
+  );
+  CREATE TABLE menus
+(
+  id                INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  restaurant_id     INTEGER  NOT NULL,
+  name              VARCHAR  NOT NULL,
+  cookname          VARCHAR  NOT NULL,
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (id) ON DELETE CASCADE ON UPDATE CASCADE
+  );
 CREATE TABLE meals
 (
   id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  menu_id     INTEGER  NOT NULL,
   price       INTEGER  NOT NULL,
-  name        VARCHAR  NOT NULL
-  );
-CREATE TABLE menus
-(
-  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-
   name        VARCHAR  NOT NULL,
-  cookname    VARCHAR  NOT NULL
+  FOREIGN KEY (menu_id) REFERENCES menus (id) ON DELETE CASCADE ON UPDATE CASCADE
   );
 
-CREATE TABLE menumeals
-(
-  id           bigint PRIMARY KEY DEFAULT nextval('global_seq'),
-  menu_id    INTEGER  NOT NULL ,
-  meal_id    INTEGER  NOT NULL ,
 
 
-  CONSTRAINT fk_menumeals_menu FOREIGN KEY (menu_id) REFERENCES menus (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_menumeals_meal FOREIGN KEY (meal_id) REFERENCES meals (id) ON DELETE CASCADE ON UPDATE CASCADE
-
-  );
