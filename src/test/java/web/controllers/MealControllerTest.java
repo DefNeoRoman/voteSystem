c3a43aef10aed054cbad6a308fc7df3740561b29
DefNeoRoman <d89056411060@yandex.ru>
2017-12-19 00:35:16 +0300

    why?

--- src/test/java/web/controllers/MealControllerTest.java
+++ src/test/java/web/controllers/MealControllerTest.java
@@ -33,7 +33,6 @@ public class MealControllerTest {
     public void getAll() throws Exception {
         mvc.perform(get("/admin/meals/getAll"))
                 .andExpect(status().isOk())
-
                 .andExpect(content().contentType(IntegrationTestUtil.APPLICATION_JSON_UTF8))
                 .andExpect(content().string(
                         "[{\"mealId\":1,\"mealName\":\"макароны\",\"price\":763,\"menuId\":1,\"menuName\":\"завтрак\",\"menuNames\":[],\"menuIds\":[]},{\"mealId\":2,\"mealName\":\"картофель\",\"price\":863,\"menuId\":2,\"menuName\":\"обед\",\"menuNames\":[],\"menuIds\":[]},{\"mealId\":3,\"mealName\":\"хлеб\",\"price\":773,\"menuId\":3,\"menuName\":\"ужин\",\"menuNames\":[],\"menuIds\":[]}]"));
